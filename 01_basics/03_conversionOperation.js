c3d5641aa4b2e89107c953e2c51765c2897a44c0
hardika-rajpurohit <68323651+hardika-rajpurohit@users.noreply.github.com>
2024-01-12 10:47:34 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "hardika"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// "77" => 77
+// "77abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "hardika"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hardika" => true
+
+let someNumber = 77
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
