2cd86bbf2abbb51ef1154a80d89afa73a5d5d425
hardika-rajpurohit <68323651+hardika-rajpurohit@users.noreply.github.com>
2024-01-12 11:09:38 +0000

    conversion practice

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,5 +23,37 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 77
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ************* Operations **************** 
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " hardika"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// read about prefix and postfix operator
