7670e4039d33ac910e84dfa2d2421c2adcf0cf34
hardika-rajpurohit <68323651+hardika-rajpurohit@users.noreply.github.com>
2024-01-13 08:52:04 +0000

    json understanding

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -51,10 +51,38 @@ const users = [
 ] // multiple objects in array
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+const course = {
+    coursename: "js",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+// const {courseInstructor} = course
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// JSON
+
+{
+    "name": "hitesh"
+    "coursename": "js",
+   " price": "free",
+}
+
+[
+    {},
+    {},
+    {}
+]
