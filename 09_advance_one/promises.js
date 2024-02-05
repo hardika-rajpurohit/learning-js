01963fac99d046ba1676cbce33dd8a84f64d0b3c
hardika-rajpurohit <68323651+hardika-rajpurohit@users.noreply.github.com>
2024-02-05 17:10:51 +0530

    Update promises

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -1,5 +1,5 @@
 const promiseOne = new Promise(function(resolve, reject){
-    //Do an async task
+    // Do an async task
     // DB calls, cryptography, network
     setTimeout(function(){
         console.log('Async task is compelete');
